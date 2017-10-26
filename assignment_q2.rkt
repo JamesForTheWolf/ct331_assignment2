da8449c3e7332f84800fd0cbc08c01820dcd7899
James Diamond <32022347+JamesForTheWolf@users.noreply.github.com>
2017-10-26 23:27:13 +0100

    Add files via upload

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,33 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
+
 
 (provide ins_beg)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (cons el lst))
+
++(ins_beg '1 (list 2 3 4))
++(ins_beg '(a b) '(b c d))
+
+;inserting at end
+(provide ins_end)
+(define (ins_end el lst)
+  (append lst (list el)))
+
++(ins_end 'a '(b c d))
++(ins_end '(a b) '(b c d))
+
+(provide count_top_level)
+;counting top level items
+(define (count_top_level lst) 
+  (if (null? lst) 0
+      (+1 (count_top_level (cdr lst)))))
+
+;counting instances of item in list
+(provide count_instances)
+(define (count_instances item lst)
+  (cond[(null? lst) 0]
+       [(equal? item (car lst)) (+1 (count_instances item lst 0))]
+       [else (count_instances item (cdr lst))]))
+
